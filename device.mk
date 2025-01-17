ae2eba1359e5e4f448832bc43fa4dc483559a698
LuK1337 <priv.luk@gmail.com>
2025-01-17 10:27:55 +0100

    pdx246: Build QTI USB HAL
    Change-Id: Id502964e8508534c84cc03cae3662ff2728d98a7

--- device.mk
+++ device.mk
@@ -178,6 +178,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    usb_compositions.conf
+
+PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
+
 # WiFi firmware symlink
 PRODUCT_PACKAGES += \
     firmware_wlanmdsp.otaupdate_symlink \
