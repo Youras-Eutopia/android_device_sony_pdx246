e8e15c0482c9804764724e2d1e922acfd4b3b714
LuK1337 <priv.luk@gmail.com>
2025-01-17 10:25:24 +0100

    pdx246: Build fingerprint HAL
    Change-Id: I639a283ccde7fe48dd5719adcb8350f1912a4eaa

--- device.mk
+++ device.mk
@@ -80,6 +80,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint-service.lineage
+
 # Partitions
 PRODUCT_PACKAGES += \
     vendor_bt_firmware_mountpoint \
