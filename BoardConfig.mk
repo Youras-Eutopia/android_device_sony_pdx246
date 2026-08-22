dd367683ba1bffda623f0fbe5439ee1062749b18
Dongyeong Choi <milosix3413@gmail.com>
2026-08-22 02:11:15 +0000

    pdx246: Configure architecture for parrot
    Change-Id: I89959fb28e5b3fab3b8a2ca94a407584f8d340c9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,5 +5,18 @@
 
 DEVICE_PATH := device/sony/pdx246
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := kryo300
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a75
+
 # Inherit the proprietary files
 include vendor/sony/pdx246/BoardConfigVendor.mk
